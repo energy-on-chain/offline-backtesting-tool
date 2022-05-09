355bebb1e4fbac4d8685268534c0c0a7cfd6c268
hartimat <matthew.t.hartigan@gmail.com>
2022-05-09 10:23:43 -0500

    Updated template file again

--- strategy_template.R
+++ strategy_template.R
@@ -3,26 +3,46 @@
 # CLIENT: Chainview Capital
 # AUTHOR: Matt Hartigan
 # DATE CREATED: 8 May 2022
-# DESCRIPTION: Template file to use for developing a trading strategy that is
-# compatible with the Chainview Offline Backtesting system.
+# DESCRIPTION: Copy and paste this file for a head start when developing a 
+# a new trading strategy that is compatible with the CVC Offline Back Testing 
+# system.
 ###############################################################################
 
 
 
-# (1) IMPORT & CLEAN DATA
+# (1) AUTHENTICATE
 
 
-# (2) APPLY MACHINE LEARNING
+# (2) CONFIG
+threshold = 0.6
+bet = 10000    # usd
 
 
-# (3) APPLY TRADING STRATEGY LOGIC
+# (3) IMPORT & CLEAN DATA
+# TODO: connect coinbase to gcf
+# TODO: connect falconx to gcf
 
 
-# (4) EVALUATE PERFORMANCE & OUTPUT RESULTS
+# (4) APPLY MACHINE LEARNING
+# TODO: add
+
+
+# (5) APPLY TRADING STRATEGY LOGIC
+# TODO: add
+
+
+# (6) EVALUATE PERFORMANCE & OUTPUT RESULTS
+# TODO: Implement performance function library (e.g. sharpe ratio, ...)
+# TODO: Implement plotting feature
+# TODO: Output results to google cloud
 
 
 
-
-
-# TODO
+# TODO:
 # Are we going to version control the strategy files?
+# Are we going to keep everything in one single file?
+# Add to automated email system
+# Can we rewrite the bot exchange connections in R?
+
+
+
