e149c92a7cb1ab1c15d365a506bf5fe2d39da161
hartimat <matthew.t.hartigan@gmail.com>
2022-05-09 10:22:17 -0500

    Initial commit of strategy template file

---
+++ strategy_template.R
@@ -0,0 +1,28 @@
+###############################################################################
+# FILENAME: strategy_template.R
+# CLIENT: Chainview Capital
+# AUTHOR: Matt Hartigan
+# DATE CREATED: 8 May 2022
+# DESCRIPTION: Template file to use for developing a trading strategy that is
+# compatible with the Chainview Offline Backtesting system.
+###############################################################################
+
+
+
+# (1) IMPORT & CLEAN DATA
+
+
+# (2) APPLY MACHINE LEARNING
+
+
+# (3) APPLY TRADING STRATEGY LOGIC
+
+
+# (4) EVALUATE PERFORMANCE & OUTPUT RESULTS
+
+
+
+
+
+# TODO
+# Are we going to version control the strategy files?
